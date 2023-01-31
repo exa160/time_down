e421c4b0ad56db6474e3c56c1ff68d3bfa884435
exa160 <sfy111@hotmail.com>
2023-01-31 23:54:42 +0800

    update count down

--- timer.py
+++ timer.py
@@ -1,37 +1,78 @@
 import time
 from tkinter import Tk, Label
+from datetime import datetime, timedelta
+
+
+def get_hours(weekday):
+    if weekday in (0, 1, 3):
+        return 20
+    else:
+        return 17
+
+def start_time():
+    now = datetime.now()
+    set_datetime = datetime(now.year, now.month, now.day, get_hours(now.weekday()), 30, 0)
+    if now > set_datetime:
+        set_datetime = datetime(now.year, now.month, now.day, get_hours(now.weekday()+1 if now.weekday() < 4 else 0), 30, 0) + timedelta(days=1 if now.weekday() < 4 else 7 - now.weekday())
+    return set_datetime
+
+
+def diff_time_cal(set_datetime):
+    now = datetime.now()
+    diff = set_datetime - now
+    day = diff.days
+    hour = diff.seconds // 3600 + day *3600 * 24
+    minute = (diff.seconds % 3600) // 60
+    second = (diff.seconds % 3600) % 60
+    return now, hour, minute, second
 
 
 class TimeShow():  # 实现倒计时
 
-    def __init__(self, time_show=5):
+    def __init__(self):
         self.timeShowWin = Tk()
         self.timeShowWin.overrideredirect(True)
         self.timeShowWin.attributes('-alpha', 1)
         self.timeShowWin.attributes('-topmost', True)
         self.timeShowWin.attributes('-transparentcolor', 'red')
-        self.time_show = time_show
-        self.time_label = Label(self.timeShowWin, text='倒计时{}秒'.format(
-            self.time_show), font=('楷体', 12), fg='black', bg='red')
+        self.time_label = Label(self.timeShowWin, text='倒计时', font=('楷体', 12), fg='black', bg='red')
         self.time_label.pack(fill='x', anchor='center')
         self.timeShowWin.geometry(
-            '+'+str(int(self.timeShowWin.winfo_screenwidth() - 180))+'+'+str(int(self.timeShowWin.winfo_screenheight() - 65)))
+            '+'+str(int(self.timeShowWin.winfo_screenwidth() - 130))+'+'+str(int(self.timeShowWin.winfo_screenheight() - 66)))
         self.timeShowWin.after(1, self.show)
 
     def show(self):
-        while self.time_show >= 0:
-            print('time_label={}'.format(self.time_label))
-            self.time_label['text'] = '倒计时{}秒'.format(self.time_show)
-            self.timeShowWin.update()
-            self.time_show -= 1
-            time.sleep(1)
+        act_index = ['-', '\\', '|', '/']
+        temp_index = 0
+        start_datetime = start_time()
+        while True:
+            now, hour, minute, second = diff_time_cal(start_datetime)
+            if now.hour <= get_hours(now.weekday()):
+                index = int(f'{now.microsecond:06d}'[:2]) // 25
+                if temp_index != index:
+                    temp_index = index
+                    self.time_label['text'] = f'{hour:02d}: {minute:02d}{":" if index > 1 else " "} {second:02d} {act_index[index]}'
+                    self.timeShowWin.update()
+                time.sleep(0.25)
+            else:
+                index = now.second % 20 // 5
+            #     start_datetime = start_time()
+                if temp_index != index:
+                    temp_index = index
+                    self.time_label['text'] = f'--: --{":" if now.second // 2 == 0 else " "} -- {act_index[index]}\n'\
+                                              f'{hour:02d}: {minute:02d}-nextD'
+                    # self.time_label['text'] = f'{hour:02d}: {minute:02d}{":" if index > 1 else " "} {second:02d} {act_index[index]}'
+                    self.timeShowWin.update()
+                time.sleep(5)
+
+
         self.timeShowWin.destroy()
 
     def start(self):
-        print('ok')
         self.timeShowWin.mainloop()
 
 
 if __name__ == '__main__':
-    a = TimeShow(10)
+    print(start_time())
+    a = TimeShow()
     a.start()
