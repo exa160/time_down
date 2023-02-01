cde1110cdddcbb147c15b586592b289ccfa0da93
exa160 <sfy111@hotmail.com>
2023-02-01 23:45:47 +0800

    right click menu

--- timer.py
+++ timer.py
@@ -1,5 +1,6 @@
+import threading
 import time
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Menu
 from datetime import datetime, timedelta
 
 
@@ -9,11 +10,13 @@ def get_hours(weekday):
     else:
         return 17
 
+
 def start_time():
     now = datetime.now()
     set_datetime = datetime(now.year, now.month, now.day, get_hours(now.weekday()), 30, 0)
     if now > set_datetime:
-        set_datetime = datetime(now.year, now.month, now.day, get_hours(now.weekday()+1 if now.weekday() < 4 else 0), 30, 0) + timedelta(days=1 if now.weekday() < 4 else 7 - now.weekday())
+        set_datetime = datetime(now.year, now.month, now.day, get_hours(now.weekday() + 1 if now.weekday() < 4 else 0),
+                                30, 0) + timedelta(days=1 if now.weekday() < 4 else 7 - now.weekday())
     return set_datetime
 
 
@@ -21,13 +24,13 @@ def diff_time_cal(set_datetime):
     now = datetime.now()
     diff = set_datetime - now
     day = diff.days
-    hour = diff.seconds // 3600 + day *3600 * 24
+    hour = diff.seconds // 3600 + day * 3600 * 24
     minute = (diff.seconds % 3600) // 60
     second = (diff.seconds % 3600) % 60
     return now, hour, minute, second
 
 
-class TimeShow():  # 实现倒计时
+class TimeShow:  # 实现倒计时
 
     def __init__(self):
         self.timeShowWin = Tk()
@@ -35,41 +38,62 @@ class TimeShow():  # 实现倒计时
         self.timeShowWin.attributes('-alpha', 1)
         self.timeShowWin.attributes('-topmost', True)
         self.timeShowWin.attributes('-transparentcolor', 'red')
-        self.time_label = Label(self.timeShowWin, text='倒计时', font=('楷体', 12), fg='black', bg='red')
+        self.time_label = Label(self.timeShowWin, text='倒计时', font=('楷体', 8), fg='black', bg='red')
         self.time_label.pack(fill='x', anchor='center')
+
+        menu = Menu(self.time_label, tearoff='off')
+        menu.add_cascade(label='设置')
+        menu.add_command(label='退出', command=self.timeShowWin.destroy)
+        self.show_menu(menu)
         self.timeShowWin.geometry(
-            '+'+str(int(self.timeShowWin.winfo_screenwidth() - 130))+'+'+str(int(self.timeShowWin.winfo_screenheight() - 66)))
-        self.timeShowWin.after(1, self.show)
+            '+' + str(int(self.timeShowWin.winfo_screenwidth() - 130)) + '+' + str(
+                int(self.timeShowWin.winfo_screenheight() - 63)))
+
+        t = threading.Thread(target=self.show)
+        t.setDaemon(True)
+        t.start()
 
     def show(self):
         act_index = ['-', '\\', '|', '/']
         temp_index = 0
-        start_datetime = start_time()
+        last_start_datetime = start_time()
+        next_start_datetime = last_start_datetime
         while True:
-            now, hour, minute, second = diff_time_cal(start_datetime)
+            if next_start_datetime != last_start_datetime:
+                last_start_datetime = next_start_datetime
+            now, hour, minute, second = diff_time_cal(next_start_datetime)
             if now.hour <= get_hours(now.weekday()):
                 index = int(f'{now.microsecond:06d}'[:2]) // 25
                 if temp_index != index:
                     temp_index = index
-                    self.time_label['text'] = f'{hour:02d}: {minute:02d}{":" if index > 1 else " "} {second:02d} {act_index[index]}'
+                    self.time_label[
+                        'text'] = f'{hour:02d}: {minute:02d}{":" if index > 1 else " "} {second:02d} {act_index[index]}'
                     self.timeShowWin.update()
-                time.sleep(0.25)
+                time.sleep(0.1)
             else:
+                if next_start_datetime == last_start_datetime:
+                    next_start_datetime = start_time()
+                    now, hour, minute, second = diff_time_cal(next_start_datetime)
+
                 index = now.second % 20 // 5
-            #     start_datetime = start_time()
+                #     start_datetime = start_time()
                 if temp_index != index:
                     temp_index = index
-                    self.time_label['text'] = f'--: --{":" if now.second // 2 == 0 else " "} -- {act_index[index]}\n'\
+                    self.time_label['text'] = f'--: --{":" if now.second // 2 == 0 else " "} -- {act_index[index]}\n' \
                                               f'{hour:02d}: {minute:02d}-nextD'
                     # self.time_label['text'] = f'{hour:02d}: {minute:02d}{":" if index > 1 else " "} {second:02d} {act_index[index]}'
                     self.timeShowWin.update()
                 time.sleep(5)
 
-
-        self.timeShowWin.destroy()
-
     def start(self):
         self.timeShowWin.mainloop()
+
+    def show_menu(self, menu):
+        def set_menu(event):
+            menu.post(event.x + self.timeShowWin.winfo_rootx(), event.y + self.timeShowWin.winfo_rooty())
+            self.timeShowWin.update()
+
+        self.time_label.bind('<Button-3>', set_menu)
 
 
 if __name__ == '__main__':
